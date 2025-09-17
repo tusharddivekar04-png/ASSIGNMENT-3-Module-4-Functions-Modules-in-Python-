1a95d8bd1b90ef5c5561daf11f1889a6a6cb2912
tusharddivekar04-png <tusharddivekar04@gmail.com>
2025-09-17 13:27:46 +0530

    Add files via upload

---
+++ asssign4.py
@@ -0,0 +1,14 @@
+def factorial(n):
+    # Check for valid input
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        result = 1
+        for i in range(2, n + 1):
+            result *= i
+        return result
+# Call the function with a sample number
+number = 5
+print(f"The factorial of {number} is: {factorial(number)}")
